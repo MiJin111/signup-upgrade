c29559394969ca33be0739496ee278e635cf4e71
MiJin111 <wnalwls201@naver.com>
2021-06-04 22:04:49 +0900

    final

--- crud/urls.py
+++ crud/urls.py
@@ -23,4 +23,8 @@ urlpatterns = [
     path('<str:id>',detail,name="detail"),
     path('new/',new,name="new"),
     path('create/',create,name = "create"),
+    path('edit/<str:id>',edit, name="edit"),
+    path('update/<str:id>',update, name="update"),
+    path('delete/<str:id>',delete, name = 'delete'),
+    path('house/<str:id>',house, name = 'house'),
 ]
--- signup/views.py
+++ signup/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render,redirect,get_object_or_404
+from django.utils import timezone
 from .models import Signup
 # Create your views here.
 
@@ -20,5 +21,29 @@ def create(request):
     new_signup.pub_date = request.POST['pub_date']
     new_signup.email = request.POST['email']
     new_signup.introduce = request.POST['introduce']
+    new_signup.pub_date = timezone.now()
     new_signup.save()
     return redirect('detail',new_signup.id)
+
+def edit(request,id):
+    edit_signup = Signup.objects.get(id= id)
+    return render(request,'edit.html',{'signup':edit_signup})
+
+def update(request,id):
+    update_signup = Signup.objects.get(id = id)
+    update_signup.name = request.POST['name']
+    update_signup.age = request.POST['age']
+    update_signup.pub_date = request.POST['pub_date']
+    update_signup.email = request.POST['email']
+    update_signup.introduce = request.POST['introduce']
+    update_signup.save()
+    return redirect('detail',update_signup.id)
+
+def delete(request,id):
+    delete_signup = Signup.objects.get(id=id)
+    delete_signup.delete()
+    return redirect('home')
+
+
+def house(request,id):
+    return redirect('home')
